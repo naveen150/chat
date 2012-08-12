b57672d90e7bce9ab8adaa68b3fcf7ac3fd69453
Seth Hayward <seth.hayward@gmail.com>
2012-08-11 21:20:31 -0400

    Added a fix to support full screen content height... seems to work in firefox

--- client.js
+++ client.js
@@ -436,6 +436,9 @@ function who () {
 
 $(document).ready(function() {
 
+  $(window).bind("orientationchange resize pageshow", fixgeometry);
+
+
   //submit new messages when the user hits enter if the message isnt blank
   $("#entry").keypress(function (e) {
     if (e.keyCode != 13 /* Return */) return;
@@ -503,6 +506,26 @@ $(document).ready(function() {
 
   showConnect();
 });
+
+//
+
+  var fixgeometry = function() {
+    /* Some orientation changes leave the scroll position at something
+     * that isn't 0,0. This is annoying for user experience. */
+    scroll(0, 0);
+
+    /* Calculate the geometry that our content area should take */
+    var header = $(".header:visible");
+    var footer = $(".footer:visible");
+    var content = $(".content:visible");
+    var viewport_height = $(window).height();
+    
+    var content_height = viewport_height - header.outerHeight() - footer.outerHeight();
+    
+    /* Trim margin/border/padding height */
+    content_height -= (content.outerHeight() - content.height());
+    content.height(content_height);
+  }; /* fixgeometry */
 
 //if we can, notify the server that we're going away.
 $(window).unload(function () {
